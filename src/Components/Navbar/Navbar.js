b21e0c5e44d6d9272532e8b3ca509a3d189e73b8
ashkan20171 <ashkan.mataee@yahoo.com>
2021-09-24 17:29:38 +0330

    edit navbar , ...

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 function Navbar() {
+    const[menuState,setMenuState] = useState(false)
+    const menuHandler = () =>setMenuState(!menuState)
     return (
         <nav className="navbar">
             <div className="navbar-container">
             <Link to="/" className="navbar-logo">
                 Travel  <i class="fab fa-typo3"></i>
             </Link>
+              <div className="menu-icon" onClick={menuHandler}>
+              <i className={menuState ?'fas fa-times' : 'fas fa-bars'} />
+
+               </div>   
             </div>
         </nav>
     )
