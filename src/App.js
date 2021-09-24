4825546ef1ecf52ad6f8be748e675053d44a3f6c
ashkan20171 <ashkan.mataee@yahoo.com>
2021-09-24 16:00:41 +0330

    add icon , edit navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/pages/Home'
 function App() {
   return (
     <>
       <Router>
        <Navbar />
+       <Switch>
+         <Route path="/" exact component={Home} />
+       </Switch>
       </Router>
     </>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Navbar.css'
 function Navbar() {
     return (
         <nav className="navbar">
-            
+            <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+                Travel  <i class="fab fa-typo3"></i>
+            </Link>
+            </div>
         </nav>
     )
 }
